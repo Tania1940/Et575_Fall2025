3d3b42c993bcc58c9c6005e0132fc8d9d529461f
Tania1940 <158099343+Tania1940@users.noreply.github.com>
2025-10-18 02:54:56 +0000

    comeple

--- lab10_nestedloop/lab10_Akthar.cpp
+++ lab10_nestedloop/lab10_Akthar.cpp
@@ -61,7 +61,30 @@ int main(){
         cout<<endl;
     }
 
-    cout<<"\n----- E -----"<<endl;
+    cout<<"\n----- Exercise -----"<<endl;
+    //A)
+    int sum=0, count=0;
+    for (int n = 20; n> 0; n-=2){
+        if (n%3 ==0){
+            count++;
+            continue;
+        }
+        sum += n;
+    }
+    cout<<"The sum is: "<<sum<<endl<<"The count is: "<<count<<endl;
+    //B)
+    int i=0, add=0;
+    while(i<=10){
+        i++;
+        if(i<5 && i !=2){   
+            cout<<"i = "<<i<<endl;
+            }
+        else{continue;}
+        add +=i;
+    }
+    cout<<"Add = "<<add<<endl;
+
+
 
     return 0;
 }
