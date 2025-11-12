231d02a141d36143513926a0fd041ae32385f1b2
Tania1940 <158099343+Tania1940@users.noreply.github.com>
2025-11-12 20:54:29 +0000

    compele lab 15

---
+++ lab15/lab15_main_Akthar.cpp
@@ -0,0 +1,36 @@
+/*
+Tania Akthar
+Nov 12, 2025
+lab 14: introduction to array, pointers and references
+*/
+
+#include<iostream>
+using namespace std;
+
+#include "lab15_function_Akthar.cpp"
+
+int main(){
+    cout<<"\n ----- Example 1: pointers -----"<<endl;
+    pointer_ref();
+
+    cout<<"\n ----- Example 2: pointers and reference in a function -----"<<endl;
+    string something = "Hello World!";
+    printvalue(something);
+    printbyreference(something);
+    printaddress(&something);
+    printvalue(something);
+
+    cout<<"\n ----- Example 3: array -----"<<endl;
+    arrays();
+
+    cout<<"\n ----- Example 4: array sizes -----"<<endl;
+    sizeofarrays();
+
+    cout<<"\n ----- Example 5: loop in an array -----"<<endl;
+    looparray();
+
+    cout<<"\n ----- EXEERCISE ----- "<<endl;
+    exercise();
+
+    return 0;
+}
